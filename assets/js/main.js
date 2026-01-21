23a8d6c861122e6ae69562e44b5b23f58071f68c
notoamrmustafa <omarmustafa2024@gmail.com>
2026-01-21 09:35:33 -0500

    date change

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,14 +29,14 @@
     /* ==========================================================================
       countdown timer
       ========================================================================== */
-    jQuery("#clock").countdown("2026/1/17", function (event) {
+    jQuery("#clock").countdown("2030/1/17", function (event) {
       var $this = jQuery(this).html(
         event.strftime(
           "" +
-            '<div class="time-entry days"><span>%-D</span> Days</div> ' +
-            '<div class="time-entry hours"><span>%H</span> Hours</div> ' +
-            '<div class="time-entry minutes"><span>%M</span> Minutes</div> ' +
-            '<div class="time-entry seconds"><span>%S</span> Seconds</div> '
+          '<div class="time-entry days"><span>%-D</span> Days</div> ' +
+          '<div class="time-entry hours"><span>%H</span> Hours</div> ' +
+          '<div class="time-entry minutes"><span>%M</span> Minutes</div> ' +
+          '<div class="time-entry seconds"><span>%S</span> Seconds</div> '
         )
       );
     });
